c2ee3b93de795afe6a97011df7b06f1a98b1a50e
Apurbalal Singha <apurbalal@users.noreply.github.com>
2023-05-26 22:14:33 +0900

    Fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const removeImports = require("next-remove-imports")();
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   experimental: { esmExternals: true },
   reactStrictMode: true,
+  assetPrefix: isProd ? '/browser-code' : undefined,
 }
 
 module.exports = removeImports(nextConfig);
